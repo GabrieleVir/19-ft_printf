b5e1c2d33d3581bc46eb5b29483ea97f9f465ff3
Gabriele <gabriele@MacBook-Air-de-Gabriele.local>
2018-10-08 20:55:45 +0200

    started the .h and the .c

---
+++ ft_printf.h
@@ -0,0 +1,15 @@
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+# include <locale.h>
+# include <stdlib.h>
+
+typedef struct		s_flist
+{
+	char		flags;
+	
+}			t_flist
+
+#endif
