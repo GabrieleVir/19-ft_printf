5db7f5739ddd73624eb8a8abcb9be75f94576f0a
Gabriele <gvirga@student.s19.be>
2018-10-18 20:48:53 +0200

    interesting tests done

--- tests/main_nb_bases.c
+++ tests/main_nb_bases.c
@@ -6,7 +6,7 @@
 /*   By: gvirga <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/18 15:34:22 by gvirga            #+#    #+#             */
-/*   Updated: 2018/10/18 20:04:40 by gabriele         ###   ########.fr       */
+/*   Updated: 2018/10/18 20:48:08 by gabriele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,30 @@ int		main(void)
 {
 	char	*str;
 
-	str = ft_itoa_base(-588, 10);
-	printf("%d\n", -588);
-	printf("%s\n", str);
+	// Testing the octal conversion
+	printf("======testing the value 07777======\n");
+	str = ft_itoa_base(07777, 8);
+	printf("Printf %o\n", 07777);
+	printf("My itoa base: %s\n", str);
+	printf("======testing the value 100======\n");
+	str = ft_itoa_base(100, 8);
+	printf("Printf %o\n", 100);
+	printf("My itoa base: %s\n", str);
+	printf("======testing the value -07777=====\n");
+	str = ft_itoa_base(-07777, 8);
+	printf("Printf %o\n", -07777);
+	printf("My itoa base: %s\n", str);
+	printf("=====testing the value -100=====\n");
+	str = ft_itoa_base(-100, 8);
+	printf("Printf %o\n", -100);
+	printf("My itoa base: %s\n", str);
+	printf("=====testing the value INT MAX=====\n");
+	str = ft_itoa_base(2147483648, 8);
+	printf("Printf %o\n", 2147483648);
+	printf("My itoa base: %s\n", str);
+	printf("=====testing the value INT MIN=====\n");
+	str = ft_itoa_base(-2147483648, 10);
+	printf("Printf %o\n", -2147483648);
+	printf("My itoa base: %s\n", str);
 	return (0);
 }
